ffc8b75f818d47ba9e753d6052f93c04d6c8ff95
Ruan Pablo Medeiros <pm.ruan@gmail.com>
2022-10-06 20:51:21 -0300

    chore: config styled-components theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
-  return <h1>Hello World!</h1>
+  return (
+    <>
+      <ThemeProvider theme={defaultTheme}>
+        <h1>TESTE</h1>
+      </ThemeProvider>
+    </>
+  )
 }
