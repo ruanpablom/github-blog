99eabce63d08e5b81cd6de360bfd16c65525a480
Ruan Pablo Medeiros <pm.ruan@gmail.com>
2022-10-06 21:57:49 -0300

    chore: fonts configuration

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,4 +11,10 @@ export const GlobalStyle = createGlobalStyle`
     background-color: ${(props) => props.theme['base-background']};
     color: ${(props) => props.theme['base-text']}
   }
+
+  body, input, span, h1, h2, h3, h4, h5, h6, strong, button {
+    font-family: 'Nunito', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
 `
